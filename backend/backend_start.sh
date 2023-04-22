c86071b1a7ee18080a8ad76ef42b2a0ec209134c
independencia-datalake <independencia.datalake@gmail.com>
2023-04-22 15:53:19 +0000

    update

--- backend/backend_start.sh
+++ backend/backend_start.sh
@@ -1,8 +1,8 @@
 # ! /bin/sh
-python manage.py makemigrations
-python manage.py migrate
-python manage.py createsuperuser
-python manage.py loaddata database/fixtures/CI.json
-python manage.py loaddata database/fixtures/UV.json
-python manage.py loaddata database/fixtures/CD.json
-python manage.py loaddata database/fixtures/D.json
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py createsuperuser
+python3 manage.py loaddata database/fixtures/CI.json
+python3 manage.py loaddata database/fixtures/UV.json
+python3 manage.py loaddata database/fixtures/CD.json
+python3 manage.py loaddata database/fixtures/D.json
