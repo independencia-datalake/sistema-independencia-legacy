4dfe7de84dd3c749dfb661ff7dad8890ddd42bdc
Octavio Montt <63523896+Chacon-tu-drema@users.noreply.github.com>
2023-04-23 17:36:01 +0100

    frontend dev

---
+++ frontend/src/environment/environment.dev.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    title: 'Developer Environment Heading',
+    apiURL: 'http://54.207.142.208/api'
+  };
--- frontend/src/environment/environment.prod.ts
+++ frontend/src/environment/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     title: 'Production Environment Heading',
-    apiURL: 'http://54.207.142.208/api'
+    apiURL: ''
   };
