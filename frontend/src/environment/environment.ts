509113c47ccafbf7f948738bbad89dfe3f031e7a
Paula Alarcon <paulaalarconbernal@gmail.com>
2023-06-12 13:18:35 -0400

    agerga elementos en front

--- frontend/src/environment/environment.ts
+++ frontend/src/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: false,
-    title: 'Local Environment Heading',
-    apiURL: 'http://localhost:8000/api'
-  };
+  production: false,
+  title: 'Local Environment Heading',
+  apiURL: ' http://127.0.0.1:8000/api'
+};
